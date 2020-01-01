c3c915c782150979c4991b046ff67da263e46d48
mac <wenbin.li01@ucarinc.com>
2020-01-01 21:35:12 +0800

    初始化工程

---
+++ LWBScrollerViewTest/LWBScrollerViewTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LWBScrollerViewTest
+//
+//  Created by mac on 2020/1/1.
+//  Copyright © 2020 lwb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ LWBScrollerViewTest/LWBScrollerViewTest/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  LWBScrollerViewTest
+//
+//  Created by mac on 2020/1/1.
+//  Copyright © 2020 lwb. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor redColor];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
